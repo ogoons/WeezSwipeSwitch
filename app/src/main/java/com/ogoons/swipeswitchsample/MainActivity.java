19ab0d5abf4fdfa41c0f054453b2416ce761bec3
ogoons <ogoons@hanmail.net>
2017-01-20 15:31:25 +0900

    Sample code modified

--- app/src/main/java/com/ogoons/swipeswitchsample/MainActivity.java
+++ app/src/main/java/com/ogoons/swipeswitchsample/MainActivity.java
@@ -8,17 +8,15 @@ import android.widget.Toast;
 import com.ogoons.swipeswitchsample.widget.WeezSwipeSwitch;
 
 public class MainActivity extends AppCompatActivity {
-    private WeezSwipeSwitch mSwipeSwitch;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSwipeSwitch = (WeezSwipeSwitch) findViewById(R.id.swipe_switch);
-        mSwipeSwitch.setSwipeOverlayColor(100, ContextCompat.getColor(this, R.color.colorAccent));
-        mSwipeSwitch.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
-        mSwipeSwitch.setOnSwipeSwitchListener(new WeezSwipeSwitch.OnSwipeSwitchListener() {
+        WeezSwipeSwitch swipeSwitch = (WeezSwipeSwitch) findViewById(R.id.swipe_switch);
+        swipeSwitch.setSwipeOverlayColor(100, ContextCompat.getColor(this, R.color.colorAccent));
+        swipeSwitch.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        swipeSwitch.setOnSwipeSwitchListener(new WeezSwipeSwitch.OnSwipeSwitchListener() {
             @Override
             public void onSwitchOn() {
                 Toast.makeText(getApplicationContext(), "Succeed", Toast.LENGTH_SHORT).show();
